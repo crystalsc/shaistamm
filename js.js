3560ccd1657176d347c8d926352dc5086302e23f
Crystal Conroy <csconroy@udel.edu>
2019-08-29 21:02:53 -0400

    starting commit

---
+++ js.js
@@ -0,0 +1,48 @@
+// change navbar color when scrolling
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".navbar");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+});
+// smooth scroll above anchor
+$(document).ready(function () {
+    $('a[href^="#"]').click(function () {
+        var target = $(this.hash);
+        if (target.length == 0) target = $('a[name="' + this.hash.substr(1) + '"]');
+        if (target.length == 0) target = $('html');
+        $('html, body').animate({ scrollTop: target.offset().top - 100 }, 1000);
+        return false;
+    });
+});
+// change active tab while scrolling
+$(window).scroll(function () {
+    var scrollDistance = $(window).scrollTop();
+
+    // Assign active class to nav links while scolling
+    $('.page-section').each(function (i) {
+        if ($(this).position().top <= scrollDistance + 200) {
+            $('.navigation a').parent().removeClass('active');
+            $('.navigation a').eq(i).parent().addClass('active');
+        }
+    });
+});
+
+$(document).ready(function () {
+
+    $("#sidebar").mCustomScrollbar({
+         theme: "minimal"
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        // open or close navbar
+        $('#sidebar').toggleClass('active');
+        // close dropdowns
+        $('.collapse.in').toggleClass('in');
+        // and also adjust aria-expanded attributes we use for the open/closed arrows
+        // in our CSS
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+
+});
+
